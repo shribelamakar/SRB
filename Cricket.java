e57ba8f85909e208c7b420fcb6a4231c60e5c293
shribelamakar <72966406+shribelamakar@users.noreply.github.com>
2020-10-19 23:24:06 +0530

    Add files via upload

---
+++ Cricket.java
@@ -0,0 +1,31 @@
+public class Cricket
+	{
+	public static void main(String[] args)
+		{
+		System.out.println("Bat & Bowl");
+		byte wicketsPerMatch;
+			wicketsPerMatch=20;
+		short ballsPerMatch;
+			ballsPerMatch=120;
+		int RunsScoredByATeamInADecade;
+			RunsScoredByATeamInADecade=1476457;	
+		long totalRunsInCricket;
+			totalRunsInCricket=1522569962;
+		float strikeRateOfBatsman;
+			strikeRateOfBatsman=251.693f;
+		double areaOfGround;
+			areaOfGround=1000.25836256956d;
+		boolean itsOut;
+			itsOut=false;
+		char key;
+			key='S';
+		System.out.println(wicketsPerMatch);
+		System.out.println(ballsPerMatch);
+		System.out.println(RunsScoredByATeamInADecade);
+		System.out.println(totalRunsInCricket);
+		System.out.println(strikeRateOfBatsman);
+		System.out.println(areaOfGround);
+		System.out.println(itsOut);
+		System.out.println(key);
+		}
+	}
