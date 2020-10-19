db1efaf180bcb794435c6919826af8ddc5ba1fca
shribelamakar <72966406+shribelamakar@users.noreply.github.com>
2020-10-19 23:41:24 +0530

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,30 @@
+public class Library
+	{
+	public static void main(String[] args)
+		{
+		byte tables;
+			tables=10;
+		short persons;
+			persons=500;
+		int totalNumberOfBooks;
+			totalNumberOfBooks=156457;	
+		long totalNumberOfPages;
+			totalNumberOfPages=132569962;
+		float thicknessOfPaper;
+			thicknessOfPaper=0.005f;
+		double areaOfLibrary;
+			areaOfLibrary=00.25836256956d;
+		boolean logIn;
+			logIn=false;
+		char key;
+			key='A';
+		System.out.println(tables);
+		System.out.println(persons);
+		System.out.println(totalNumberOfBooks);
+		System.out.println(totalNumberOfPages);
+		System.out.println(thicknessOfPaper);
+		System.out.println(areaOfLibrary);
+		System.out.println(logIn);
+		System.out.println(key);
+		}
+	}
