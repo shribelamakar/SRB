aee1907527a856036243f3e8c76d86111ea7be6a
shribelamakar <72966406+shribelamakar@users.noreply.github.com>
2020-10-19 23:43:08 +0530

    Add files via upload

---
+++ Water.java
@@ -0,0 +1,31 @@
+public class Water
+	{
+	public static void main(String[] args)
+		{
+		System.out.println("Water");
+		byte oceans;
+			oceans=4;
+		short seas;
+			seas=197;
+		int rivers;
+			rivers=1000007;	
+		long borewells;
+			 borewells=1000000962;
+		float volume;
+			volume=10000.123f;
+		double area;
+			area=1.000000000000005;
+		boolean noWater;
+			noWater=false;
+		char key;
+			key='S';
+		System.out.println(oceans);
+		System.out.println(seas);
+		System.out.println(rivers);
+		System.out.println(borewells);
+		System.out.println(volume);
+		System.out.println(area);
+		System.out.println(noWater);
+		System.out.println(key);
+		}
+	}
