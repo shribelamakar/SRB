adb46f7cde73bd3a9169207350f422664e01edb5
shribelamakar <72966406+shribelamakar@users.noreply.github.com>
2020-10-19 23:42:23 +0530

    Add files via upload

---
+++ Solarsystem.java
@@ -0,0 +1,31 @@
+public class Solarsystem
+	{
+	public static void main(String[] args)
+		{
+		System.out.println("Solarsystem");
+		byte planets;
+			planets=8;
+		short moons;
+			moons=100;
+		int asteroids;
+			asteroids=26325457;	
+		long stars;
+			stars=136569962;
+		float weightOfSatellite;
+			weightOfSatellite=5263.2365f;
+		double areaOfSolarsystem;
+			areaOfSolarsystem=100260.25836256956d;
+		boolean itsNight;
+			itsNight=true;
+		char key;
+			key='S';
+		System.out.println(planets);
+		System.out.println(moons);
+		System.out.println(asteroids);
+		System.out.println(stars);
+		System.out.println(weightOfSatellite);
+		System.out.println(areaOfSolarsystem);
+		System.out.println(itsNight);
+		System.out.println(key);
+		}
+	}
