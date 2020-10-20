359cb1a5ae8c03efa20f3efc0c3c9827ffc95fe3
shribelamakar <72966406+shribelamakar@users.noreply.github.com>
2020-10-20 22:32:19 +0530

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,14 @@
+public class Details
+	{
+	public static void main(String[] args)
+		{
+		final String aadharNumber="01234567890";
+		System.out.println("aadharNumber: " +aadharNumber);
+		final String mobileNumber="7847073830";
+		System.out.println("mobileNumber: " +mobileNumber);
+		final String email="srb.xworkz@gmail.com";
+		System.out.println("email: " +email);
+		}
+	}
+
+
