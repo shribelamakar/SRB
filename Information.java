bd8bdabc7810668ccdc768093993f5a7fe2ebe37
shribelamakar <72966406+shribelamakar@users.noreply.github.com>
2020-10-20 22:33:35 +0530

    Add files via upload

---
+++ Information.java
@@ -0,0 +1,16 @@
+public class Information
+	{
+	public static void main(String[] args)
+		{
+		email("srb.xworkz@gmail.com");
+		programmingLanguage("Java");
+		}
+	public static void email( String email)
+		{
+		System.out.println("email :"+email);
+		}
+	public static void programmingLanguage( String programmingLanguage)
+		{
+		System.out.println("programmingLanguage :"+programmingLanguage);
+		}
+	}
