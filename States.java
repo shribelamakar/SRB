6e60b2805f4bcd517befba0105a93827f9cf4bee
shribelamakar <72966406+shribelamakar@users.noreply.github.com>
2020-10-22 21:54:02 +0530

    Add files via upload

---
+++ States.java
@@ -0,0 +1,29 @@
+public class States
+	{		
+	public static void main(String[] state)
+		{		
+		System.out.println("Its on states");
+		System.out.println("Invoking states");
+
+		int size=state.length;
+		System.out.println("Number of states here are: "+state.length);
+		
+		System.out.println("State  @ index 0 "+state[0]);
+		System.out.println("State  @ index 1 "+state[1]);
+		System.out.println("State  @ index 2 "+state[2]);
+		System.out.println("State  @ index 3 "+state[3]);
+		System.out.println("State  @ index 4 "+state[4]);
+		System.out.println("State  @ index 5 "+state[5]);
+		System.out.println("State  @ index 6 "+state[6]);
+		System.out.println("State  @ index 7 "+state[7]);
+		System.out.println("State  @ index 8 "+state[8]);
+		System.out.println("State  @ index 9 "+state[9]);
+		System.out.println("State  @ index 10 "+state[10]);
+		System.out.println("State  @ index 11 "+state[11]);
+		System.out.println("State  @ index 12 "+state[12]);
+		System.out.println("State  @ index 13 "+state[13]);
+		System.out.println("State  @ index 14 "+state[14]);
+
+		System.out.println("Exit");
+		}
+	}
