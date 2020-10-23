1174af2281e3659228109b6c1bfc602c86fc1a74
shribelamakar <72966406+shribelamakar@users.noreply.github.com>
2020-10-23 22:40:47 +0530

    Add files via upload

---
+++ Shop.java
@@ -0,0 +1,10 @@
+public class Shop
+	{
+	public static void vehicle(String[] bike)
+	{
+	int sizeOfMainParam=bike.length;
+	System.out.println("sizeOfMainParam " +sizeOfMainParam);
+	System.out.println("Element @ index 0 "+bike[0]);
+	System.out.println("Element @ index 1 "+bike[1]);
+	}
+}
