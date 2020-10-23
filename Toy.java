eb55b59f9bdc3e61472c6a343bb8229adcb1a1a8
shribelamakar <72966406+shribelamakar@users.noreply.github.com>
2020-10-23 22:40:06 +0530

    Add files via upload

---
+++ Toy.java
@@ -0,0 +1,10 @@
+public class Toy
+	{
+	public static void main(String[] game)	
+		{
+		System.out.println("JVM invoked main");
+		Pubg.supply();
+		Pubg.enemiesAhead();
+		System.out.println("exit main");
+		}
+	}
