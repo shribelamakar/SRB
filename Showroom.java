21ee52a2251b9907ef54f1cd9fe7847c7ded1abd
shribelamakar <72966406+shribelamakar@users.noreply.github.com>
2020-10-23 22:41:19 +0530

    Add files via upload

---
+++ Showroom.java
@@ -0,0 +1,11 @@
+public class Showroom
+	{
+	public static void main(String[] bike)
+		{
+		System.out.println("JVM invoked main");
+		//print refernce representation
+		Shop.vehicle(bike);
+		System.out.println("bike "+ bike);
+		System.out.println("exit main");
+		}
+	}
