c1bad083c99f6b3011a90054870625b83d92d052
shribelamakar <72966406+shribelamakar@users.noreply.github.com>
2020-10-29 20:16:21 +0530

    Add files via upload

---
+++ iPhone/IphoneTester.java
@@ -0,0 +1,12 @@
+public class IPhoneTester
+	{
+	public static void main(String[] models)
+		{
+		System.out.println("JVM invoked main ");		
+		IPhone.call();
+		System.out.println("return value from call");
+		String sandesha=IPhone.message();
+		System.out.println("return value from message() "+ sandesha);
+		System.out.println("EXIT:: main");
+		}
+	}
