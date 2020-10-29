a9da8c2c7e0570314ed69e3092f3978e3ca5bddb
shribelamakar <72966406+shribelamakar@users.noreply.github.com>
2020-10-29 20:18:51 +0530

    Add files via upload

---
+++ Result/PercentagePredictor.java
@@ -0,0 +1,12 @@
+public class PercentagePredictor
+	{
+	public static void main(String[] marks)
+		{
+		System.out.println("JVM invoked main");
+		String score=Result.report(100);
+	
+		System.out.println("Performance based on percentage is:" +score);
+		
+        		System.out.println("EXIT:: main");
+		}
+	}
