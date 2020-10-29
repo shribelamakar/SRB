d7e6457ae27aded0cef6c2cb5eb62f609f3ccdb8
shribelamakar <72966406+shribelamakar@users.noreply.github.com>
2020-10-29 20:13:47 +0530

    Add files via upload

---
+++ Age/AgePredictor.java
@@ -0,0 +1,9 @@
+public class AgePredictor
+	{
+	public static void main(String[] person)
+		{
+		System.out.println("JVM invoked main");
+		String ageValue=Age.AgeDefine(65);
+		System.out.println("The person with this age is  "+ageValue);
+		}
+	}
